e65f0a2d5deb11ca0d43255afc830124d1c3ba27
Sven Frenzel <sven@frenzel.dk>
2015-07-18 18:06:04 +0200

    fixed FAR issues

--- modpack.go
+++ modpack.go
@@ -16,7 +16,7 @@ func main() {
     os.Exit(1)
   }
 
-  fmt.Println("\nThis is v4.0.1-beta of the ksp2mars modpack script.\n")
+  fmt.Println("\nThis is v4.0.5-beta of the ksp2mars modpack script.\n")
 
   // Start with mod installation
   if (inputArguments.BeautyFlag) {
