3444e299e2c0a0865a750d1b80c03e0a90a50549
Sven Frenzel <sven@frenzel.dk>
2015-08-18 22:50:47 +0200

    SpaceY dockingport

--- libraries/installsteps/installsteps.go
+++ libraries/installsteps/installsteps.go
@@ -201,6 +201,7 @@ func MoveMods(relevantPaths *Paths) error {
     "/EngineLighting/EngineLight/GameData",
     "/ImprovedChaseCam",
     "/PlanetShine/Alternate Colors/Real Solar System",
+    "/SpaceY-Lifters",
   }
   if err := customMoveMods(relevantPaths, customFolders); err != nil {
     return err
